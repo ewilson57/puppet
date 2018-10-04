537650545f6f88db37494762123ea61a94e016ad
Earl Wilson <ewilson57@gmail.com>
2018-10-03 21:09:14 -0400

    fixed sudo

--- manifests/user-sudo.pp
+++ manifests/user-sudo.pp
@@ -1,9 +1,11 @@
-sudo::rule { "extra_rule":
+include sudo
+
+sudo::rule { 'ewilson_sudo':
   ensure   => present,
   who      => 'ewilson',
   runas    => 'root', # default runas user is root, please change to override.
-  commands => "/usr/sbin/systemctl",
+  commands => '/opt/puppetlabs/bin/puppet',
   nopass   => false, #
   setenv   => false,
-  comment  => "what ever you like",
+  comment  => 'Puppet administrator',
 }
