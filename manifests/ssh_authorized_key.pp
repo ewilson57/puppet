d0d56810d1ce203a04856b54651acb1461f1f2af
Earl Wilson <ewilson57@gmail.com>
2018-10-14 22:25:50 +0000

    removed ssh_authorized_key.pp

--- manifests/ssh_authorized_key.pp
+++
@@ -1,5 +0,0 @@
-ssh_authorized_key { 'ubuntu@ubuntu-xenial':
-  user => 'ubuntu',
-  type => 'ssh-rsa',
-  key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDq1ha4gKWshlbNUnDq7Qhu8RZbRtgDl8YegSRZGCCF7u3Ieda7uFlnrX3b+cxQSUvF+XIPZhNWgFKA91XfcBJzF3A0pI5n4ZBIDXm/bg5ePj4COfJmWzgxkhkx3aOm3BJ+AtY4IK8WO0w48unqRzfAhIjmYgOjPhRaql2F5sTHxFw0ZgQTPVJshZQBUOiKZUJzUtgRZlg8MmsoddJnftaC8tZz0ChmJX5aiuvNy/AdT4apbPzwvlabTgZ0JAGE6Jz+KmD45xQiFLc2TAVP4FvgAhuZ4yDUGNvujaSyVjKNJ4qemCZhpWfp3XZffE80TGGY9lnS4jV1jiu07DyzQi3j',
-}
